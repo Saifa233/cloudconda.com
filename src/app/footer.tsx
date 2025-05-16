c82fa4c367f308b7178d7da116c6e2b58197862c
Saifa233 <saifazeem99@gmail.com>
2025-05-16 19:21:36 +0500

    Footer bugs fixed

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -23,23 +23,20 @@ export default function Footer(){
 
     return(
         <div style={{
-            backgroundImage: 'url(https://images.unsplash.com/photo-1707209857266-fa0eb4c3b05d?q=80&w=2113&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);background-size:cover;background-position:center;background-repeat:no-repeat)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat'
-            }} className="">
+                backgroundImage: 'url(https://images.unsplash.com/photo-1707209857266-fa0eb4c3b05d?q=80&w=2113&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat'
+            }}className="">
             <section className="px-10 xl:px-18 pt-20 pb-14 xl:pb-24">
                 <div className="flex justify-between">
-                <div className="text-white text-lg flex flex-col gap-8 xl:gap-16 2xl:gap-12">
-                    <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToServices} className="text-md 2xl:text-2xl">Services</span></div>
-                    <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToJoinUs} className="text-md 2xl:text-2xl">Join Us</span></div>
-                    <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToInsights} className="text-md 2xl:text-2xl">Insights</span></div>
-                    <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToContact} className="text-md 2xl:text-2xl">Get In Touch</span></div>
-                    <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToOffices} className="text-md 2xl:text-2xl">Offices</span></div>
-                </div>
-                <div className="flex items-end">
-                    <Link href="/" className="block"><img className="mx-2 lg:mx-4 2xl:h-[150px] 2xl:w-[200px] 2xl:pt-12 2xl:translate-y-18" src="/logo-og.png"></img></Link>
-                </div>
+                    <div className="text-white text-lg flex flex-col gap-8 xl:gap-16 2xl:gap-6">
+                        <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToServices} className="text-md 2xl:text-2xl">Services</span></div>
+                        <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToJoinUs} className="text-md 2xl:text-2xl">Join Us</span></div>
+                        <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToInsights} className="text-md 2xl:text-2xl">Insights</span></div>
+                        <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToContact} className="text-md 2xl:text-2xl">Get In Touch</span></div>
+                        <div className="flex gap-4"><span className="text-md 2xl:text-xl">→</span><span onClick={goToOffices} className="text-md 2xl:text-2xl">Offices</span></div>
+                    </div>
                 </div>
             </section>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,17 @@ import './globals.css'
 import NavBar from './navbar';
 import Footer from './footer';
 import Form from './form';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 // cloudconda.com
 export default function App(){
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
   const router = useRouter();
 
   const goToContact = () => {
@@ -97,7 +104,7 @@ export default function App(){
         <section className="p-0">
           <div className="flex flex-col gap-8">
             <div style={{
-                backgroundImage: 'url(https://images.unsplash.com/photo-1707209857266-fa0eb4c3b05d?q=80&w=2113&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);background-size:cover;background-position:center;background-repeat:no-repeat)',
+                backgroundImage: 'url(https://images.unsplash.com/photo-1707209857266-fa0eb4c3b05d?q=80&w=2113&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat'
@@ -150,7 +157,7 @@ export default function App(){
       </div>
 
       <div style={{
-        backgroundImage: 'url(https://images.unsplash.com/photo-1707209857266-fa0eb4c3b05d?q=80&w=2113&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);background-size:cover;background-position:center;background-repeat:no-repeat)',
+        backgroundImage: 'url(https://images.unsplash.com/photo-1707209857266-fa0eb4c3b05d?q=80&w=2113&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat'
@@ -174,7 +181,7 @@ export default function App(){
 
       <Form/>
       
-      <Footer/>
+      {isClient && <Footer/>}
     </div>
     );
   }
